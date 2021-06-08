983d8ce6f5259947cb9539b05a29ec2c432f61dc
danaremar <danaremar@alum.us.es>
2021-06-08 17:09:44 +0200

    Update intall-java.cmd

--- certificates/intall-java.cmd
+++ certificates/intall-java.cmd
@@ -1,3 +1,5 @@
 "C:\Program Files\Java\jdk-11.0.1\bin\keytool.exe" -import -trustcacerts -alias root -file "C:\Users\elsen\Documents\GitHub\pai5-st17\certificates\certificate.pem" -keystore "C:\Program Files\Java\jdk-11.0.1\lib\security\cacerts" -storepass changeit
 
 "C:\Program Files\Java\jdk-11.0.1\bin\keytool.exe" -list -keystore "C:\Program Files\Java\jdk-11.0.1\lib\security\cacerts"
+
+"C:\Program Files\Java\jdk-11.0.1\bin\keytool.exe" -importcert -file "C:\Users\elsen\Documents\GitHub\pai5-st17\certificates\certificate.pem" -keystore "C:\Users\elsen\Documents\GitHub\pai5-st17\certificates\certificate.jks" -storepass jkspass -alias st17 -keypass st17
